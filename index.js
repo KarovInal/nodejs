a8dce8dbdb7e8f0583f0465895cd76bc9af88fa6
KarovInal <inal.karov8@gmail.com>
2023-06-30 15:32:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const { launch, getStream } = require('puppeteer-stream')
 const { exec } = require('child_process')
-const {executablePath} = require('puppeteer')
+
+const child_process = require('child_process');
+
+const getExecutablePath = () => {
+  if (process.env.CHROME_BIN) {
+    return process.env.CHROME_BIN;
+  }
+
+  let executablePath;
+  if (process.platform === 'linux') {
+    try {
+      executablePath = child_process.execSync('which chromium-browser').toString().split('\n').shift();
+    } catch (e) {
+      // NOOP
+    }
+
+    if (!executablePath) {
+      executablePath = child_process.execSync('which chromium').toString().split('\n').shift();
+      if (!executablePath) {
+        throw new Error('Chromium not found (which chromium)');
+      }
+    }
+  } else if (process.platform === 'darwin') {
+    executablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+  } else if (process.platform === 'win32') {
+    executablePath = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
+  } else {
+    throw new Error('Unsupported platform: ' + process.platform);
+  }
+
+  return executablePath;
+};
 
 // puppeteer stream + FFmpeg example: https://github.com/Flam3rboy/puppeteer-stream/blob/main/examples/ffmpeg.js
 
@@ -12,14 +43,20 @@ let twitch =
   'rtmps://dc4-1.rtmp.t.me/s/1714692839:9KI2gwSXzfuL3JI65HtbyQ'
 
 async function test() {
-  const browser = await launch();
+  const browser = await launch({
+    executablePath: getExecutablePath(),
+		defaultViewport: {
+			width: 1920,
+			height: 1080,
+		},
+	});
 
   const page = await browser.newPage()
   await page.goto('https://nuto.education')
   const stream = await getStream(page, {
     audio: true,
     video: true,
-    frameSize: 1000,
+    frameSize: 30,
   })
   console.log('recording')
   // this will pipe the stream to ffmpeg and convert the webm to mp4 format
